c8904b134cfdb3e06cd3e21c143b04d0d77313d8
Emmanuel <agbavweisaac@gmail.com>
2021-08-08 19:49:21 +0100

    Added an infinite progress indicator to the login page on click of the login button with the right account info entered.

--- src/aroshop/DBManagement.java
+++ src/aroshop/DBManagement.java
@@ -11,6 +11,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -21,201 +22,227 @@ import javafx.util.Duration;
 
 public class DBManagement {
 
-	static String password;
+    static String password;
 
-	static String error = "Error!";
+    static String error = "Error!";
 
-	static String errorMessage = "Some kind of error occurred mate";
+    static String errorMessage = "Some kind of error occurred mate";
 
-	static String jdbcUrl = "jdbc:sqlite:src/Database/AroShop.db";
+    static String jdbcUrl = "jdbc:sqlite:src/Database/AroShop.db";
+    static ProgressIndicator pi = new ProgressIndicator();
 
-	// Working methods
-	public static void valUser() {
-		try ( Connection connect = DriverManager.getConnection(jdbcUrl)) {
+    // Working methods
+    public static void valUser() {
+        try (Connection connect = DriverManager.getConnection(jdbcUrl)) {
 
-			String sql = "SELECT * FROM userinfo WHERE username = ? AND password = ?;";
-			PreparedStatement statement = connect.prepareStatement(sql);
+            String sql = "SELECT * FROM userinfo WHERE username = ? AND password = ?;";
+            PreparedStatement statement = connect.prepareStatement(sql);
 
-			statement.setString(1, AroShop.loginPage.getUsernameField().getText());
-			statement.setString(2, AroShop.loginPage.getPassField().getText());
+            statement.setString(1, AroShop.loginPage.getUsernameField().getText());
+            statement.setString(2, AroShop.loginPage.getPassField().getText());
 
-			ResultSet result = statement.executeQuery();
+            ResultSet result = statement.executeQuery();
 
-			if (result.next()) {
-				alert("Login Success!", "Welcome " + AroShop.userName);
-				if (!AroShop.userName.equals(AroShop.loginPage.previousUser)) {
-					AroShop.newLook.loadCart();
-					AroShop.newLook.loadPurchases();
-				}
-				if (AroShop.newLook.orderBox.getChildren().isEmpty()) {
-					AroShop.newLook.order.setDisable(true);
-				}
-				if (AroShop.newLook.cartBox.getChildren().isEmpty()) {
-					AroShop.newLook.cart.setDisable(true);
-				}
+            if (result.next()) {
+                pi.setMaxSize(100, 100);
+                AroShop.loginPage.pane.getChildren().add(pi);
+                AroShop.loginPage.usernameField.setEditable(false);
+                AroShop.loginPage.passField.setEditable(false);
+                AroShop.loginPage.loginBtn.setDisable(true);
+                AroShop.loginPage.fgtPassBtn.setDisable(true);
+                AroShop.loginPage.signUpBtn.setDisable(true);
 
-				AroShop.viewNew();
+                AroShop.loginPage.previousUser = AroShop.userName;
+                AroShop.setUsername();
+                if (!AroShop.userName.equals(AroShop.loginPage.previousUser)) {
+                    AroShop.newLook.loadCart();
+                    AroShop.newLook.loadPurchases();
+                }
+                if (AroShop.newLook.orderBox.getChildren().isEmpty()) {
+                    AroShop.newLook.order.setDisable(true);
+                }
+                if (AroShop.newLook.cartBox.getChildren().isEmpty()) {
+                    AroShop.newLook.cart.setDisable(true);
+                }
 
-				FadeTransition fadeIn = new FadeTransition(Duration.seconds(2), AroShop.newLook.lay);
-				fadeIn.setFromValue(0);
-				fadeIn.setToValue(1);
-				fadeIn.setCycleCount(1);
-				fadeIn.play();
+                FadeTransition delay = new FadeTransition(Duration.seconds(2), AroShop.loginPage.pane);
+                delay.setFromValue(1);
+                delay.setToValue(1);
+                delay.setCycleCount(1);
 
-			} else {
-				AroShop.loginPage.passField.setText("");
-				alert("Wrong Password!", "Forgot password?", "Click the 'Forgot Password?' button");
-			}
-		} catch (SQLException ex) {
-			alert(error, errorMessage);
-		}
-	}
+                FadeTransition fadeIn = new FadeTransition(Duration.seconds(2), AroShop.newLook.lay);
+                fadeIn.setFromValue(0);
+                fadeIn.setToValue(1);
+                fadeIn.setCycleCount(1);
 
-	public static void getPassword() {
-		try ( Connection connect = DriverManager.getConnection(jdbcUrl)) {
-			String sql2 = "SELECT * FROM userinfo WHERE username = ?;";
-			PreparedStatement statement2 = connect.prepareStatement(sql2);
+                delay.play();
 
-			statement2.setString(1, AroShop.forgotPassword.getUsernameField().getText());
+                delay.setOnFinished(e -> {
+                    AroShop.loginPage.pane.getChildren().remove(pi);
+                    fadeIn.play();
+                    AroShop.viewNew();
+                    AroShop.loginPage.usernameField.setEditable(true);
+                    AroShop.loginPage.passField.setEditable(true);
+                    AroShop.loginPage.loginBtn.setDisable(false);
+                    AroShop.loginPage.fgtPassBtn.setDisable(false);
+                    AroShop.loginPage.signUpBtn.setDisable(false);
+                    alert("Login Success!", "Welcome " + AroShop.userName);
+                });
 
-			ResultSet result2 = statement2.executeQuery();
+            } else {
+                AroShop.loginPage.passField.setText("");
+                alert("Wrong Password!", "You seem to have entered a wrong password");
+            }
+        } catch (SQLException ex) {
+            alert(error, errorMessage);
+        }
+    }
 
-			if (result2.next()) {
-				PreparedStatement statement;
-				try ( Connection connect1 = DriverManager.getConnection(jdbcUrl)) {
+    public static void getPassword() {
+        try (Connection connect = DriverManager.getConnection(jdbcUrl)) {
+            String sql2 = "SELECT * FROM userinfo WHERE username = ?;";
+            PreparedStatement statement2 = connect.prepareStatement(sql2);
 
-					String sql = "SELECT password FROM userinfo WHERE question = ? AND answer = ?;";
-					statement = connect1.prepareStatement(sql);
+            statement2.setString(1, AroShop.forgotPassword.getUsernameField().getText());
 
-					statement.setString(1, AroShop.forgotPassword.getQuestions().getValue());
-					statement.setString(2, AroShop.forgotPassword.getAnswer().getText());
+            ResultSet result2 = statement2.executeQuery();
 
-					ResultSet result = statement.executeQuery();
+            if (result2.next()) {
+                PreparedStatement statement;
+                try (Connection connect1 = DriverManager.getConnection(jdbcUrl)) {
 
-					if (result.next()) {
-						password = result2.getString("password");
-						alert("Retrieval Successful!", "Your password is '" + password + "'", "Please store it in a safe but accessible location");
-						AroShop.viewLogin();
-						AroShop.loginPage.usernameField.setText(AroShop.forgotPassword.getUsernameField().getText());
-					} else {
-						alert("Account Not Found!", "Sorry the information provided don't match");
-					}
-				} catch (Exception e) {
-				}
-			} else {
-				alert("Account Not Found!", "The username you provided could not be found", "Please make sure your username is correct");
-			}
-		} catch (SQLException ex) {
-			alert(error, errorMessage);
-		}
-	}
+                    String sql = "SELECT password FROM userinfo WHERE question = ? AND answer = ?;";
+                    statement = connect1.prepareStatement(sql);
 
-	protected static void signUp() {
-		PreparedStatement pstmt;
-		try ( Connection connection = DriverManager.getConnection(jdbcUrl)) {
-			String sql = "INSERT INTO userinfo (username, password, age, sex, question, answer) VALUES(?,?,?,?,?,?);";
-			pstmt = connection.prepareStatement(sql);
+                    statement.setString(1, AroShop.forgotPassword.getQuestions().getValue());
+                    statement.setString(2, AroShop.forgotPassword.getAnswer().getText());
 
-			pstmt.setString(1, AroShop.signUp.getUsernameField().getText());
-			pstmt.setString(2, AroShop.signUp.getPassword().getText());
-			pstmt.setInt(3, Integer.parseInt(AroShop.signUp.getAgeField().getText()));
-			pstmt.setString(4, AroShop.signUp.getSexComboBox().getValue());
-			pstmt.setString(5, AroShop.signUp.getQuestions().getValue());
-			pstmt.setString(6, AroShop.signUp.getAnswer().getText());
+                    ResultSet result = statement.executeQuery();
 
-			pstmt.executeUpdate();
+                    if (result.next()) {
+                        password = result2.getString("password");
+                        alert("Retrieval Successful!", "Your password is '" + password + "'", "Please store it in a safe but accessible location");
+                        AroShop.viewLogin();
+                        AroShop.loginPage.usernameField.setText(AroShop.forgotPassword.getUsernameField().getText());
+                    } else {
+                        alert("Account Not Found!", "Sorry the information provided don't match");
+                    }
+                } catch (Exception e) {
+                }
+            } else {
+                alert("Account Not Found!", "The username you provided could not be found", "Please make sure your username is correct");
+            }
+        } catch (SQLException ex) {
+            alert(error, errorMessage);
+        }
+    }
 
-			AroShop.viewLogin();
-			AroShop.loginPage.usernameField.setText(AroShop.signUp.getUsernameField().getText());
-			AroShop.loginPage.passField.setText(AroShop.signUp.getPassword().getText());
-			alert("Sign Up Successful!", "You can now login to make purchases");
-		} catch (SQLException e) {
-			alert(error, errorMessage);
-		}
-	}
-	// End of working methods
+    protected static void signUp() {
+        PreparedStatement pstmt;
+        try (Connection connection = DriverManager.getConnection(jdbcUrl)) {
+            String sql = "INSERT INTO userinfo (username, password, age, sex, question, answer) VALUES(?,?,?,?,?,?);";
+            pstmt = connection.prepareStatement(sql);
 
-	// Alerts 
-	protected static void alert(String title, String labelText) {
-		Stage stage = new Stage();
+            pstmt.setString(1, AroShop.signUp.getUsernameField().getText());
+            pstmt.setString(2, AroShop.signUp.getPassword().getText());
+            pstmt.setInt(3, Integer.parseInt(AroShop.signUp.getAgeField().getText()));
+            pstmt.setString(4, AroShop.signUp.getSexComboBox().getValue());
+            pstmt.setString(5, AroShop.signUp.getQuestions().getValue());
+            pstmt.setString(6, AroShop.signUp.getAnswer().getText());
 
-		BorderPane pane = new BorderPane();
+            pstmt.executeUpdate();
 
-		Label msg = new Label(labelText + ".");
-		msg.setAlignment(Pos.CENTER);
+            AroShop.viewLogin();
+            AroShop.loginPage.usernameField.setText(AroShop.signUp.getUsernameField().getText());
+            AroShop.loginPage.passField.setText(AroShop.signUp.getPassword().getText());
+            alert("Sign Up Successful!", "You can now login to make purchases");
+        } catch (SQLException e) {
+            alert(error, errorMessage);
+        }
+    }
+    // End of working methods
 
-		Button ok = new Button("Ok");
-		ok.setDefaultButton(true);
-		ok.setOnAction(e -> stage.close());
-		ok.setAlignment(Pos.CENTER);
-		ok.getStyleClass().add("alert-button");
+    // Alerts 
+    protected static void alert(String title, String labelText) {
+        Stage stage = new Stage();
 
-		HBox box = new HBox(ok);
-		box.setPadding(new Insets(10));
-		box.setAlignment(Pos.CENTER);
+        BorderPane pane = new BorderPane();
 
-		pane.setCenter(msg);
-		pane.setMargin(msg, new Insets(30, 0, 20, 0));
-		pane.setBottom(box);
+        Label msg = new Label(labelText + ".");
+        msg.setAlignment(Pos.CENTER);
 
-		Scene scene = new Scene(pane);
-		scene.getStylesheets().add("/Styling/Alerts.css");
+        Button ok = new Button("Ok");
+        ok.setDefaultButton(true);
+        ok.setOnAction(e -> stage.close());
+        ok.setAlignment(Pos.CENTER);
+        ok.getStyleClass().add("alert-button");
 
-		Image aro = new Image("/Icon/Aro.png");
+        HBox box = new HBox(ok);
+        box.setPadding(new Insets(10));
+        box.setAlignment(Pos.CENTER);
 
-		stage.getIcons().add(aro);
-		stage.initModality(Modality.APPLICATION_MODAL);
-		stage.setWidth(400);
-		stage.setResizable(false);
-		stage.setScene(scene);
-		stage.initOwner(AroShop.stage);
-		stage.centerOnScreen();
-		stage.setTitle(title);
-		stage.showAndWait();
-	}
+        pane.setCenter(msg);
+        pane.setMargin(msg, new Insets(30, 0, 20, 0));
+        pane.setBottom(box);
 
-	protected static void alert(String title, String text, String text2) {
-		Stage stage = new Stage();
+        Scene scene = new Scene(pane);
+        scene.getStylesheets().add("/Styling/Alerts.css");
 
-		BorderPane pane = new BorderPane();
+        Image aro = new Image("/Icon/Aro.png");
 
-		Label msg = new Label(text + ".");
-		msg.setAlignment(Pos.CENTER);
+        stage.getIcons().add(aro);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setWidth(400);
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.initOwner(AroShop.stage);
+        stage.centerOnScreen();
+        stage.setTitle(title);
+        stage.show();
+    }
 
-		Label msg2 = new Label(text2 + ".");
-		msg2.setAlignment(Pos.CENTER);
+    protected static void alert(String title, String text, String text2) {
+        Stage stage = new Stage();
 
-		VBox labels = new VBox(5, msg, msg2);
-		labels.setAlignment(Pos.CENTER);
+        BorderPane pane = new BorderPane();
 
-		Button ok = new Button("Ok");
-		ok.setDefaultButton(true);
-		ok.setOnAction(e -> stage.close());
-		ok.setAlignment(Pos.CENTER);
-		ok.getStyleClass().add("alert-button");
+        Label msg = new Label(text + ".");
+        msg.setAlignment(Pos.CENTER);
 
-		HBox box = new HBox(ok);
-		box.setPadding(new Insets(10));
-		box.setAlignment(Pos.CENTER);
+        Label msg2 = new Label(text2 + ".");
+        msg2.setAlignment(Pos.CENTER);
 
-		pane.setCenter(labels);
-		pane.setMargin(labels, new Insets(30, 0, 20, 0));
-		pane.setBottom(box);
+        VBox labels = new VBox(5, msg, msg2);
+        labels.setAlignment(Pos.CENTER);
 
-		Scene scene = new Scene(pane);
-		scene.getStylesheets().add("/Styling/Alerts.css");
+        Button ok = new Button("Ok");
+        ok.setDefaultButton(true);
+        ok.setOnAction(e -> stage.close());
+        ok.setAlignment(Pos.CENTER);
+        ok.getStyleClass().add("alert-button");
 
-		Image aro = new Image("/Icon/Aro.png");
+        HBox box = new HBox(ok);
+        box.setPadding(new Insets(10));
+        box.setAlignment(Pos.CENTER);
 
-		stage.getIcons().add(aro);
-		stage.initModality(Modality.APPLICATION_MODAL);
-		stage.setWidth(400);
-		stage.setResizable(false);
-		stage.setScene(scene);
-		stage.initOwner(AroShop.stage);
-		stage.centerOnScreen();
-		stage.setTitle(title);
-		stage.showAndWait();
-	}
-	// End of Alerts
+        pane.setCenter(labels);
+        pane.setMargin(labels, new Insets(30, 0, 20, 0));
+        pane.setBottom(box);
+
+        Scene scene = new Scene(pane);
+        scene.getStylesheets().add("/Styling/Alerts.css");
+
+        Image aro = new Image("/Icon/Aro.png");
+
+        stage.getIcons().add(aro);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setWidth(400);
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.initOwner(AroShop.stage);
+        stage.centerOnScreen();
+        stage.setTitle(title);
+        stage.show();
+    }
+    // End of Alerts
 }
--- src/aroshop/LoginPage.java
+++ src/aroshop/LoginPage.java
@@ -15,126 +15,126 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
 public class LoginPage {
 
-	BorderPane lay = new BorderPane();
+    BorderPane lay = new BorderPane();
 
-	Label welcome;
+    Label welcome;
 
-	Button loginBtn;
-	Button signUpBtn;
-	Button fgtPassBtn;
+    Button loginBtn;
+    Button signUpBtn;
+    Button fgtPassBtn;
 
-	protected TextField usernameField;
+    protected StackPane pane;
 
-	protected PasswordField passField;
+    protected TextField usernameField;
 
-	protected String previousUser;
+    protected PasswordField passField;
 
-	public LoginPage(BorderPane lay) {
+    protected String previousUser;
 
-		welcome = new Label("AroShop");
-		welcome.getStyleClass().add("welcome-label");
+    public LoginPage(BorderPane lay) {
 
-		this.lay = lay;
+        welcome = new Label("AroShop");
+        welcome.getStyleClass().add("welcome-label");
 
-		usernameField = new TextField();
-		usernameField.setPromptText("Enter Username");
-		usernameField.setMaxWidth(300);
+        this.lay = lay;
 
-		HBox userBox = new HBox(usernameField);
-		userBox.setHgrow(usernameField, Priority.ALWAYS);
-		userBox.setAlignment(Pos.CENTER);
+        usernameField = new TextField();
+        usernameField.setPromptText("Enter Username");
+        usernameField.setMaxWidth(300);
 
-		passField = new PasswordField();
-		passField.setPromptText("Enter Password");
-		passField.setMaxWidth(300);
+        HBox userBox = new HBox(usernameField);
+        userBox.setHgrow(usernameField, Priority.ALWAYS);
+        userBox.setAlignment(Pos.CENTER);
 
-		HBox passBox = new HBox(passField);
-		passBox.setHgrow(passField, Priority.ALWAYS);
-		passBox.setAlignment(Pos.CENTER);
+        passField = new PasswordField();
+        passField.setPromptText("Enter Password");
+        passField.setMaxWidth(300);
 
-		fgtPassBtn = new Button("Forgot Password?");
-		fgtPassBtn.setOnAction(e -> AroShop.viewForPass());
+        HBox passBox = new HBox(passField);
+        passBox.setHgrow(passField, Priority.ALWAYS);
+        passBox.setAlignment(Pos.CENTER);
 
-		HBox fgtBox = new HBox(fgtPassBtn);
-		fgtBox.setMaxWidth(300);
-		fgtBox.setMargin(fgtPassBtn, new Insets(0, 0, 20, 0));
-		fgtPassBtn.setAlignment(Pos.CENTER_RIGHT);
-		fgtPassBtn.getStyleClass().add("forgot-password");
+        fgtPassBtn = new Button("Forgot Password");
+        fgtPassBtn.setOnAction(e -> AroShop.viewForPass());
 
-		loginBtn = new Button("Log In");
-		loginBtn.setOnAction(((ActionEvent event) -> {
-			if (getUsernameField().getText().isEmpty() && getPassField().getText().isEmpty()) {
-				DBManagement.alert("Complete the form!", "Please fill in your username and password");
-			} else if (getUsernameField().getText().isEmpty()) {
-				DBManagement.alert("Empty Username Field!", "Now please fill in your username");
-			} else if (getPassField().getText().isEmpty()) {
-				DBManagement.alert("Empty Password Field!", "Now please fill in your password");
-			} else {
-				String jdbcUrl = "jdbc:sqlite:src/Database/AroShop.db";
-				PreparedStatement statement2;
-				ResultSet result2;
-				try ( Connection connect = DriverManager.getConnection(jdbcUrl)) {
+        HBox fgtBox = new HBox(fgtPassBtn);
+        fgtBox.setMaxWidth(300);
+        fgtBox.setMargin(fgtPassBtn, new Insets(0, 0, 20, 0));
+        fgtPassBtn.setAlignment(Pos.CENTER_RIGHT);
+        fgtPassBtn.getStyleClass().add("forgot-password");
 
-					String sql2 = "SELECT password FROM userinfo WHERE username = ?;";
-					statement2 = connect.prepareStatement(sql2);
-					statement2.setString(1, getUsernameField().getText());
+        loginBtn = new Button("Log In");
+        loginBtn.setOnAction(((ActionEvent event) -> {
+            if (getUsernameField().getText().isEmpty() && getPassField().getText().isEmpty()) {
+                DBManagement.alert("Complete the form!", "Please fill in your username and password");
+            } else if (getUsernameField().getText().isEmpty()) {
+                DBManagement.alert("Empty Username Field!", "Now please fill in your username");
+            } else if (getPassField().getText().isEmpty()) {
+                DBManagement.alert("Empty Password Field!", "Now please fill in your password");
+            } else {
+                String jdbcUrl = "jdbc:sqlite:src/Database/AroShop.db";
+                PreparedStatement statement2;
+                ResultSet result2;
+                try (Connection connect = DriverManager.getConnection(jdbcUrl)) {
 
-					result2 = statement2.executeQuery();
+                    String sql2 = "SELECT password FROM userinfo WHERE username = ?;";
+                    statement2 = connect.prepareStatement(sql2);
+                    statement2.setString(1, getUsernameField().getText());
 
-					if (result2.next()) {
-						previousUser = AroShop.userName;
-						AroShop.setUsername();
-						DBManagement.valUser();
-					} else {
-						DBManagement.alert("Account Not Found!", "The username you provided could not be found", "Use the 'Sign Up' botton if you are new");
-					}
-				} catch (SQLException ex) {
-					DBManagement.alert(DBManagement.error, DBManagement.errorMessage);
-				}
+                    result2 = statement2.executeQuery();
 
-			}
-		}));
-		loginBtn.setDefaultButton(true);
-		loginBtn.getStyleClass().add("log-in");
+                    if (result2.next()) {
+                        DBManagement.valUser();
+                    } else {
+                        DBManagement.alert("Account Not Found!", "The username you provided could not be found");
+                    }
+                } catch (SQLException ex) {
+                    DBManagement.alert(DBManagement.error, DBManagement.errorMessage);
+                }
+            }
+        }));
+        loginBtn.setDefaultButton(true);
+        loginBtn.getStyleClass().add("log-in");
 
-		signUpBtn = new Button("Sign Up");
-		signUpBtn.setOnAction(e -> AroShop.viewSign());
-		signUpBtn.getStyleClass().add("sign-up");
+        signUpBtn = new Button("Sign Up");
+        signUpBtn.setOnAction(e -> AroShop.viewSign());
+        signUpBtn.getStyleClass().add("sign-up");
 
-		HBox logBox = new HBox(loginBtn);
-		loginBtn.setAlignment(Pos.CENTER);
+        HBox logBox = new HBox(loginBtn);
+        loginBtn.setAlignment(Pos.CENTER);
 
-		VBox logInBox = new VBox(5, welcome, userBox, passBox, fgtBox, logBox, signUpBtn);
-		fgtBox.setAlignment(Pos.CENTER_RIGHT);
-		logBox.setAlignment(Pos.CENTER);
-		logInBox.setMargin(welcome, new Insets(0, 0, 50, 0));
-		logInBox.setMargin(signUpBtn, new Insets(50, 0, 0, 0));
+        VBox logInBox = new VBox(5, welcome, userBox, passBox, fgtBox, logBox, signUpBtn);
+        fgtBox.setAlignment(Pos.CENTER_RIGHT);
+        logBox.setAlignment(Pos.CENTER);
+        logInBox.setMargin(welcome, new Insets(0, 0, 50, 0));
+        logInBox.setMargin(signUpBtn, new Insets(50, 0, 0, 0));
+        logInBox.setAlignment(Pos.CENTER);
+        logInBox.setMaxWidth(500);
 
-		lay.setCenter(logInBox);
+        pane = new StackPane(logInBox);
 
-		logInBox.setAlignment(Pos.CENTER);
-		logInBox.setMaxWidth(500);
+        lay.setCenter(pane);
+    }
 
-	}
+    public TextField getUsernameField() {
+        return usernameField;
+    }
 
-	public TextField getUsernameField() {
-		return usernameField;
-	}
+    public void setUsernameField(TextField aUsernameField) {
+        usernameField = aUsernameField;
+    }
 
-	public void setUsernameField(TextField aUsernameField) {
-		usernameField = aUsernameField;
-	}
+    public TextField getPassField() {
+        return passField;
+    }
 
-	public TextField getPassField() {
-		return passField;
-	}
-
-	public void setPassField(PasswordField aPassField) {
-		passField = aPassField;
-	}
+    public void setPassField(PasswordField aPassField) {
+        passField = aPassField;
+    }
 
 }
